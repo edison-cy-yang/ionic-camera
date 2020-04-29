6eb9b6a634ea52707c2a7a0f14cfd61a0f236c83
edison-cy-yang <edison.cy.yang@gmail.com>
2020-04-29 15:31:54 -0700

    save photos to local file system

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource, Filesystem } from '@capacitor/core';
 
 const { Camera, FileSystem, Storage } = Plugins;
 
@@ -7,6 +7,8 @@ const { Camera, FileSystem, Storage } = Plugins;
   providedIn: 'root'
 })
 export class PhotoService {
+
+  public photos: Photo[] = [];
 
   constructor() { }
 
@@ -16,5 +18,52 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    //write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+}
+
+
+interface Photo {
+  filepath: string;
+  webviewPath: string;
+  base64?: string;
 }
